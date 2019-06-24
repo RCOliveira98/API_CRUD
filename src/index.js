fd848a4c2fcea1d957f467958efcf1410d5acd29
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-24 10:19:02 -0300

    Create controllers.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const body_parser = require('body-parser');
 const app = express();
 
 app.use(body_parser.json());
-app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.urlencoded({ extended: false }));
+
+require('./controllers/authController')(app);
 
 app.listen(3000, console.log('Servidor ON'));
