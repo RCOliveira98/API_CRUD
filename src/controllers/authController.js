4abba4011059bf301f803a3b7d18c96a9ac5a913
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-28 19:38:58 -0300

    Gerando token de autenticação do user.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,22 +1,55 @@
 const express = require('express');
-
-const User = require('../models/user');
-
+const bcrypto = require('bcryptjs');
 const router = express.Router();
-
-router.post('/register', async(req, res) => {
+const jwt = require('jsonwebtoken');
+//
+const User = require('../models/user');
+const md5 = require('../config/authConfig/md5.json');
+// métodos (functions)
+function generateToken(params = {}) {
+    return jwt.sign(params, md5.secret, {
+        expiresIn: 86400
+    });
+}
+// rotas
+router.post('/register', async function(req, res) {
+    const { email } = req.body;
     try {
-        if (await User.findOne(req.body.email)) {
+        if (await User.findOne({ email }))
             return res.status(400).send('Erro: email já cadastrado.');
-        } else {
-            const user = await User.create(req.body);
-            user.password = undefined;
-            // await: espere essa parte se concretizar para continuar
-            return res.send({ user });
-        }
+
+        const user = await User.create(req.body);
+        user.password = undefined;
+        // await: espere essa parte se concretizar para continuar
+        return res.send({ user });
     } catch (err) {
+        console.log('Equipe do Café, se vira => ' + err);
         return res.status(400).send({ error: 'Falha ao se registrar' });
     }
+});
+
+router.post('/autenticar', async function(req, res) {
+
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select('+password');
+
+    if (user) {
+        if (await bcrypto.compare(password, user.password)) {
+            // user autenticado
+            user.password = undefined; // para n retornar senha
+            // token
+            return res.status(200).send({
+                user,
+                token: generateToken({ id: user.id })
+            });
+        } else {
+            return res.status(400).send('Senha inválida!');
+        }
+    } else {
+        res.status(400).send('Usuário não cadastrado');
+    }
+
+
 });
 
 module.exports = (app) => app.use('/auth', router);
--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 // detalhes da conexão
 const port = 27017;
 const uri = 'mongodb://localhost/api_one';
-mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.connect(uri, { useNewUrlParser: true })
+    .then(() => {
+        console.log("Conexão com MongoDB realizada com SUCESSO.");
+    }).catch(err => {
+        console.log('Não foi possível realizar a conexão: ' + err);
+        process.exit();
+    });
 
 // configurações
 mongoose.Promise = global.Promise;
